8facbd2109f1122be1a653da8ef0b7b581a7abde
mohammadketabi <mohammad.ketabi@gmail.com>
2022-11-11 18:09:53 -0500

    fix deploy bug

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -6,7 +6,7 @@ import rateLimiter from "express-rate-limit";
 
 const apiLimiter = rateLimiter({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 2,
+  max: 15,
   message: "Too many requests from this IP, please try again after 15 minutes",
 });
 
